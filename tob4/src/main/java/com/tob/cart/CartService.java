daa113e8019d75d3f2f0d56ef8a236e35f906bbc
pheonix0717@naver.com <pheonix0717@naver.com>
2016-01-06 21:51:25 +0900

    2016-01-06 최종 커밋 PM 09:51

--- tob4/src/main/java/com/tob/cart/CartService.java
+++ tob4/src/main/java/com/tob/cart/CartService.java
@@ -2,13 +2,18 @@ package com.tob.cart;
 
 import java.util.List;
 
+import com.tob.book.BookVO;
+
 public interface CartService {
 	
 	// 장바구니 담기
-	public int put(String bookid);
+	public CartVO put(String bookid);
 	
 	// 장바구니 목록 확인
 	public List<CartVO> getList();
+	
+	// 넘어온 책아이디로 책정보 조회
+	public BookVO searchBybookid(String bookid);
 	
 	// 장바구니 수량 변경
 	public int changeCount(int count);
